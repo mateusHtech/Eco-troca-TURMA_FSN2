369798e4dad39b644e1d6164a5f095c146fdc535
sibele-lino <sibelecaetano7@gmail.com>
2025-07-02 22:29:14 -0300

    Página de publicar item criada, com adição de input,campos de formulário, botão de publicar item e upload

---
+++ Front-end/Ecotroca_FSN2/src/pages/PublicarItem/index.jsx
@@ -0,0 +1,64 @@
+import style from './PublicarItem.module.css';
+
+function PublicarItem() {
+    return (
+        <div>
+            <nav className={style.nav}>
+                <h2>EcoTroca</h2>
+                <ul>
+                    <li><a href="/">Início</a></li>
+                    <li><a href="/publicar">Publicar Item</a></li>
+                    <li><a href="/meus-itens">Meus Itens</a></li>
+                    <li><a href="/perfil">Perfil</a></li>
+                </ul>
+            </nav>
+            <div className={style.container}>
+                <h1>Publicar um Item</h1>
+
+                <form className="form">
+
+                    {/*upload de imagem */}
+                    <div className={style.upload}>
+                        <label htmlFor="imagem">Adicionar Imagem</label>
+                        <input type="file" id="imagem" name="imagem" />
+                    </div>
+
+                    {/* Nome do item*/}
+                    <div className={style.campo}>
+                        <label htmlFor="nome">Nome do Item</label>
+                        <input type="text" id="nome" name="nome" placeholder="Ex: Camisa Preta" />
+                    </div>
+
+                    {/*Descrição*/}
+                    <div className={style.campo}>
+                        <label htmlFor="descricao">Descrição</label>
+                        <textarea id="descricao" name="descricao" placeholder="Descreva o item..."></textarea>
+                    </div>
+
+                    {/* Categoria */}
+                    <div className={style.campo}>
+                        <label htmlFor="categoria">Categoria</label>
+                        <select id="categoria" name="categoria">
+                            <option value="">Selecione uma categoria</option>
+                            <option value="roupas">Roupas</option>
+                            <option value="eletronicos">Eletrônicos</option>
+                            <option value="moveis">Móveis</option>
+                            <option value="livros">Livros</option>
+                        </select>
+                    </div>
+
+                    {/*Cidade/Bairro */}
+                    <div className={style.campo}>
+                        <label htmlFor="localizacao">Cidade/Bairro</label>
+                        <input type="text" id="localizacao" name="localizacao" placeholder="Centro, Fortaleza" />
+                    </div>
+
+                    {/*Botão */}
+                    <button type="submit" className={style.botao}>Publicar Item</button>                
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default PublicarItem; 
