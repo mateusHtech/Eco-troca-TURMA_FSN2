a93f9ef1891ae1853d2a511de3c75033d7d5ba38
EmanoelLimaaa <limaemanoe91@gmail.com>
2025-07-04 14:25:40 -0300

    corrigido import do BrowserRouter para funcionar a tela Publicar Item

--- Front-end/Ecotroca_FSN2/src/App.jsx
+++ Front-end/Ecotroca_FSN2/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PublicarItem from './pages/PublicarItem'
 
 function App() {
